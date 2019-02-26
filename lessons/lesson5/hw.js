8b3f164e9a1a27f2c0cfcc29d884014f98100d4d
Florian Rappl <florian.rappl@smapiot.com>
2019-02-26 20:59:25 +0100

    Added some more comments

--- lessons/lesson5/hw.js
+++ lessons/lesson5/hw.js
@@ -9,17 +9,23 @@ let correctValue = 8;
 // rules of our game - in a re-usable function
 function playAnotherRound(remainingLives) {
   if (remainingLives === 0) {
+    // end the game if no more lives are there
     alert("You lost - try again!");
   } else {
-    let userGuess = prompt("Please tell me a number between 0 and 10.");
+    // otherwise we need another guess from the user
+    let guessedValue = prompt("Please tell me a number between 0 and 10.");
   
-    if (userGuess > correctValue) {
+    // if we are larger or smaller we should help the user
+    if (guessedValue > correctValue) {
       alert("Too high! Lower please.");
+      // also let's try to play another round using the provided information
       playAnotherRound(remainingLives - 1);
-    } else if (userGuess < correctValue) {
+    } else if (guessedValue < correctValue) {
       alert("Too low! Higher please.");
+      // also let's try to play another round using the provided information
       playAnotherRound(remainingLives - 1);
     } else {
+      // here we only need to inform the user and end the game
       alert("You won!");
     }
   }
